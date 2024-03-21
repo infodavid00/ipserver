25d963b9bd764329d917ddd6c418a5fb3a4d2674
infodavid00 <da40au40@gmail.com>
2024-03-21 20:44:50 +0100

    hello

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (rq,rs) => {
+ const remoteAddress = rq.headers.remoteAddress;
+  const ip = rq.ip;
+  rs.status(200).json({remoteAddress, ip})
+})
+
+app.listen(3000, ()=> console.log('app running'))
